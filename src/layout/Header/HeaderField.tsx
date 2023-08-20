eb8a4a436a4e4ee4bf606a12aade5f82a7ab796b
dukekunyura <dukekunyura@gmail.com>
2023-08-20 13:16:15 +0300

    [+] add Menu

--- src/layout/Header/HeaderField.tsx
+++ src/layout/Header/HeaderField.tsx
@@ -1,29 +1,35 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Layout, Menu } from 'antd';
+import { MenuInfo } from 'rc-menu/lib/interface';
+
+const { Header } = Layout;
 
 const HeaderField: FC = () => {
+    const navigate = useNavigate();
+    const handleMenuClick = (e: MenuInfo) => {
+        console.log("click " + e.key);
+        navigate(e.key);
+    }
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/person">Person</Link>
-                </li>
-                <li>
-                    <Link to="/test">Test</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/nothing-here">Nothing Here</Link>
-                </li>
-            </ul>
-        </nav>
-
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+            <div className="demo-logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                onClick={handleMenuClick}
+            >
+                <Menu.Item key="/">Home</Menu.Item>
+                <Menu.Item key="/person">Person</Menu.Item>
+                <Menu.Item key="/test">Test</Menu.Item>
+                <Menu.Item key="/login">Login</Menu.Item>
+                <Menu.Item key="/nothing-here">Nothing Here</Menu.Item>
+            </Menu>
+        </Header>
     )
 }
 
 export default HeaderField;
+
+
